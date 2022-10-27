2378a307136b8ef27631c5df74f7601429195d06
Irvin627 <79436962+Irvin627@users.noreply.github.com>
2022-10-27 13:09:58 -0500

    Carrito

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
+import {Product, products } from '../products';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { }
+    addToCart(product: Product){
+      this.cartService.addToCart(product);
+      window.alert('¡Tu producto ha sido agregado al carrito!');
+    }
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
-  constructor() { }
 
-  ngOnInit(): void {
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
